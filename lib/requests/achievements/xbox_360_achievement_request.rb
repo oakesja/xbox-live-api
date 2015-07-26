983c822aba3158db8a2f803a39466a6f38ff6938
Jacob Oakes <jacoboakes92@gmail.com>
2015-07-26 14:21:10 -0400

    refactored xbox360achievement request

--- lib/requests/achievements/xbox_360_achievement_request.rb
+++ lib/requests/achievements/xbox_360_achievement_request.rb
@@ -2,20 +2,35 @@ require 'requests/base_request'
 require 'http_session_gateway'
 require 'json_parser/achievements/xbox_360_achievement_json_parser'
 
-
 class Xbox360AchievementRequest < BaseRequest
 
-  # TODO break up
   def for(user_id, game)
+    @parser = Xbox360AchievementJsonParser.new
+    unlocked_achievements = get_unlocked_achievements(game, user_id)
+    all_achievements = get_all_achievements(game, user_id)
+    combine_achievements(all_achievements, unlocked_achievements)
+  end
+
+  private
+
+  def get_unlocked_achievements(game, user_id)
     url = "https://achievements.xboxlive.com/users/xuid(#{user_id})/achievements?titleId=#{game.id}"
-    json = HttpSessionGateway.new.get(url, header: header_for_version(Version::XBOX_360)).body
-    parser = Xbox360AchievementJsonParser.new
-    unlocked = parser.parse_achievements_from(json)
+    get_achievements(url)
+  end
+
+  def get_all_achievements(game, user_id)
     url = "https://achievements.xboxlive.com/users/xuid(#{user_id})/titleachievements?titleId=#{game.id}"
-    json = HttpSessionGateway.new.get(url, header: header_for_version(2)).body
-    locked = parser.parse_achievements_from(json)
-    unlocked_ids = unlocked.collect(&:id)
-    locked.reject! { |achievement| unlocked_ids.include?(achievement.id) }
-    unlocked + locked
+    get_achievements(url)
+  end
+
+  def get_achievements(url)
+    json = HttpSessionGateway.new.get(url, header: header_for_version(Version::XBOX_360)).body
+    @parser.parse_achievements_from(json)
+  end
+
+  def combine_achievements(all_achievements, unlocked_achievements)
+    unlocked_ids = unlocked_achievements.collect(&:id)
+    all_achievements.reject! { |achievement| unlocked_ids.include?(achievement.id) }
+    unlocked_achievements + all_achievements
   end
 end
